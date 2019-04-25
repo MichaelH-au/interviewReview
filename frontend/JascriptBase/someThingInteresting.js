10bc6fcbfb3c78f628a8b0f5da4fa9271a161ca7
Michael <huangjkau@gmail.com>
2019-04-25 12:03:28 +1000

    same

--- frontend/JascriptBase/someThingInteresting.js
+++ frontend/JascriptBase/someThingInteresting.js
@@ -1,3 +1,6 @@
+/**
+ *  Even arr can attach extra attribute on the variable
+ * */
 let arr = []
 
 arr.test = 123
@@ -7,7 +10,21 @@ console.log(arr)
 arr.push(1)
 console.log(arr[0])
 // 1
+
 console.log(arr[1])
 // undefined
+
+console.log(arr)
+// [1, test: 123 ]
+
+arr.push({test1:222})
+console.log(arr)
+// [ 1, { test1: 222 }, test: 123 ]
+console.log(arr.length)
+// 2
+
 console.log(arr.hasOwnProperty(test))
 // test is not defined
+
+console.log(arr.test)
+// 123
