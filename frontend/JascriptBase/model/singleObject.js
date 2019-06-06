0a7679d7455ee26f7728f2d6822a017cb93426f8
Michael <huangjkau@gmail.com>
2019-06-06 10:28:59 +1000

    modify single object

--- frontend/JascriptBase/model/singleObject.js
+++ frontend/JascriptBase/model/singleObject.js
@@ -1,3 +1,7 @@
+/**
+ * it cannot be a true single object because you cannot not make constructor
+ * to a private function
+ */
 function SingleObject() {
     this.login = () => {
         console.log('userLogin')
@@ -19,3 +23,11 @@ test.login()
 let test1 = SingleObject.getInstance()
 test1.login()
 console.log(test === test1)
+
+
+/**
+ * Application
+ * jQuery $
+ * login window
+ * vuex redux (store)
+ */
