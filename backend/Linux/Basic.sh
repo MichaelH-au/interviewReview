ea39c96e46d69cf1cb130afb93bdfd14dfccd316
Michael <huangjkau@gmail.com>
2019-07-05 19:18:26 +1000

    egrep sort uniq sort

--- backend/Linux/Basic.sh
+++ backend/Linux/Basic.sh
@@ -24,3 +24,7 @@ tr A-Z a-z
 
 echo hello123|sed -E 's/([a-z]+)([0-9]{3})/\1   \2/'
 echo 123Chello123##|sed -E 's/[^a-zA-Z]+([a-zA-Z]+)([0-9]{3}).*/\1   \2/'
+echo 123aaa456bbb | egrep -v 'aaa'
+sort
+uniq -c
+sort -n
